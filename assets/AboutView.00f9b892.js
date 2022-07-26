11d039ed5b70852de1b53b22e6e320dc4ec06feb
bnichs <bnichs@users.noreply.github.com>
2022-07-26 20:20:22 +0000

    Deploying to gh-pages from @ bnichs/bnichs.github.io@a8b2c6a1680e92f9add59781468366d80e3c05ff 🚀

---
+++ assets/AboutView.00f9b892.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,a as e}from"./index.d4639987.js";const n={},_=e("h1",null,"This is an about page",-1),a=e("h1",null,"This is an about page",-1),c=[_,a];function i(l,r){return o(),s("main",null,c)}const d=t(n,[["render",i]]);export{d as default};
---
+++ assets/ResumeView.1b5e3ab0.js
@@ -0,0 +1,1 @@
+import{d as e,o as a,c as n,e as o,f as s}from"./index.d4639987.js";const m=e({__name:"ResumeView",setup(t){return(c,r)=>(a(),n("main",null,[o(s)]))}});export{m as default};
