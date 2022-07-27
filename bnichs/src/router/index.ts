773fb02574eb840b7f8b2a86b8ae1bad3ca8cd90
Ben <mail@ben-nichols.net>
2022-07-27 13:49:01 -0700

    Blog placeholder

--- bnichs/src/router/index.ts
+++ bnichs/src/router/index.ts
@@ -25,6 +25,11 @@ const router = createRouter({
       component: () => import('../views/ResumeView.vue')
     },
     {
+      path: '/blog',
+      name: 'blog',
+      component: () => import('../views/BlogView.vue')
+    },
+    {
       path: '/skills',
       name: 'skills',
       component: () => import('../views/SkillsView.vue')
---
+++ bnichs/src/views/BlogView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from '../components/WelcomeItem.vue'
+</script>
+
+<template>
+  <main>
+    <h2>
+      Coming soon
+    </h2>
+
+    <hr>
+  </main>
+</template>
+
+<style>
+
+</style>
