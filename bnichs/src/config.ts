c9a3f3bd5b3d1d6513722f756b0241628ac7bc29
Ben <mail@ben-nichols.net>
2022-07-29 12:05:00 -0700

    Page title

--- bnichs/src/config.ts
+++ bnichs/src/config.ts
@@ -4,8 +4,10 @@ const ENV = process.env.NODE_ENV
 const isDev = ENV == "development"
 
 const PAGE_TITLE_BASE = "Ben Nichols"
-const TITLE_SUFFIX = `${isDev ? " - dev" : "" }`
-const PAGE_TITLE = `${PAGE_TITLE_BASE}${TITLE_SUFFIX}`
+const TITLE_PREFIX = `${isDev ? "dev - " : "" }`
+export const PAGE_TITLE = `${TITLE_PREFIX}${PAGE_TITLE_BASE}`
+
+
 
 
 export default {
