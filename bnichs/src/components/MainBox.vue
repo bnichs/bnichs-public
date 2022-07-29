fed14082ba811b0183bb6fa14ed7df93ac9d921c
Ben <mail@ben-nichols.net>
2022-07-29 16:12:33 -0700

    Templatize MainBox

---
+++ bnichs/src/components/MainBox.vue
@@ -0,0 +1,36 @@
+<template>
+<!--    <div class="about">-->
+  <main>
+      <h2>
+        <slot name="title"></slot>
+      </h2>
+
+      <div class="main-content">
+        <slot name="content"></slot>
+      </div>
+<!--      Ben Nichols is the fucking man-->
+<!--      I specialize in bringing enterprise knowledge to small/medium sized business/-->
+
+
+<!--      <h3>Bio</h3>-->
+
+<!--      <h3>-->
+<!--        In my free time I enjoy:-->
+<!--      </h3>-->
+<!--        <ul>-->
+<!--          <li>Being in nature, preferably with my dog</li>-->
+<!--          <li>Reading: I love SciFi, existential fiction, and anything with a good twist.</li>-->
+<!--          <li>Travelling. I'd like to road trip through every state in the USA.</li>-->
+<!--        </ul>-->
+  </main>
+</template>
+
+<style>
+/*@media (min-width: 1024px) {*/
+/*  .about {*/
+/*    !*min-height: 100vh;*!*/
+/*    !*display: flex;*!*/
+/*    align-items: center;*/
+/*  }*/
+/*}*/
+</style>
