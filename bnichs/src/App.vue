e7078225b25ebe14ad2e7e0ca062238f91632b12
Ben <mail@ben-nichols.net>
2022-07-29 18:39:55 -0700

    Cleanup app

--- bnichs/src/App.vue
+++ bnichs/src/App.vue
@@ -5,11 +5,6 @@ import TitleBox from './components/TitleBox.vue'
 
 <template>
 
-  <!--  {{ this.bare }}-->
-  <!--  <div v-if="bare">-->
-  <!--    <RouterView />-->
-  <!--  </div>-->
-  <!--  <div v-else>-->
   <header>
     <div class="wrapper">
       <RouterLink to="/">
