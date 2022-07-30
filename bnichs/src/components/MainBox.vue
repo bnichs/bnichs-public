e5f4461e16cfbd958703ecd5e0c4eebaa5c0efd5
Ben <mail@ben-nichols.net>
2022-07-29 18:46:36 -0700

    Cleanup mainbox

--- bnichs/src/components/MainBox.vue
+++ bnichs/src/components/MainBox.vue
@@ -1,5 +1,4 @@
 <template>
-<!--    <div class="about">-->
   <main>
       <h2>
         <slot name="title"></slot>
@@ -8,29 +7,8 @@
       <div class="main-content">
         <slot name="content"></slot>
       </div>
-<!--      Ben Nichols is the fucking man-->
-<!--      I specialize in bringing enterprise knowledge to small/medium sized business/-->
-
-
-<!--      <h3>Bio</h3>-->
-
-<!--      <h3>-->
-<!--        In my free time I enjoy:-->
-<!--      </h3>-->
-<!--        <ul>-->
-<!--          <li>Being in nature, preferably with my dog</li>-->
-<!--          <li>Reading: I love SciFi, existential fiction, and anything with a good twist.</li>-->
-<!--          <li>Travelling. I'd like to road trip through every state in the USA.</li>-->
-<!--        </ul>-->
   </main>
 </template>
 
 <style>
-/*@media (min-width: 1024px) {*/
-/*  .about {*/
-/*    !*min-height: 100vh;*!*/
-/*    !*display: flex;*!*/
-/*    align-items: center;*/
-/*  }*/
-/*}*/
 </style>
