78549010ca89f54b6dda61a70a6299f829410ef7
bnichs <bnichs@users.noreply.github.com>
2022-07-30 01:44:15 +0000

    Deploying to gh-pages from @ bnichs/bnichs.github.io@7f2cccf5722db06b9376896c17070fcc2cc430fd 🚀

--- assets/AboutView.a4268a96.js
+++
@@ -1,1 +0,0 @@
-import{d as t,o,c as a,w as e,a as s,_ as n,b as c}from"./index.fc99e5c7.js";import{M as _}from"./MainBox.e71ec296.js";const r=c(" About Me "),f=t({__name:"AboutView",setup(i){return(m,p)=>(o(),a(_,null,{title:e(()=>[r]),content:e(()=>[s(n)]),_:1}))}});export{f as default};
