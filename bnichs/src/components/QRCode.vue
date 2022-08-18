63e18622c3aa0919a4341b5d675559b64904c2a2
Ben <mail@ben-nichols.net>
2022-08-17 18:55:49 -0700

    QRCode component

---
+++ bnichs/src/components/QRCode.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import QrcodeVue from 'qrcode.vue'
+</script>
+
+<template>
+    <qrcode-vue class="qr-box" :value="value" :size="size" level="H" />
+</template>
+
+<script lang="ts">
+import {PERSON_WEBSITE} from "@/config";
+
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 55
+    }
+  },
+  data() {
+    return {
+      value: PERSON_WEBSITE,
+      // size: 55,
+    }
+  },
+}
+</script>
+
+
+<style>
+</style>
