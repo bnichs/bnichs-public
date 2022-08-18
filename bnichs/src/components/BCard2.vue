63b9f62bf64cfeb610ea92836e2b3ed016ca9be8
Ben <mail@ben-nichols.net>
2022-08-18 09:36:38 -0700

    Cruft

--- bnichs/src/components/BCard2.vue
+++ bnichs/src/components/BCard2.vue
@@ -58,22 +58,6 @@ export default {
 }
 
 
-/*@media (prefers-color-scheme: light){*/
-/*  :root {*/
-/*    --color-background: var(--vt-c-white);*/
-/*    --color-background-soft: var(--vt-c-white-soft);*/
-/*    --color-background-mute: var(--vt-c-white-mute);*/
-
-/*    --color-border: var(--vt-c-divider-light-2);*/
-/*    --color-border-hover: var(--vt-c-divider-light-1);*/
-
-/*    --color-heading: var(--vt-c-text-light-1);*/
-/*    --color-text: var(--vt-c-text-light-1);*/
-
-/*    --section-gap: 160px;*/
-/*  }*/
-/*}*/
-
 :root{
 --card-width: 3.5in;
 --card-height: 2in;
