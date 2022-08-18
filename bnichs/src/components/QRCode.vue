5290d811e5cf01955d56618a013a89fdf665d8ec
Ben <mail@ben-nichols.net>
2022-08-18 09:37:05 -0700

    QR color

--- bnichs/src/components/QRCode.vue
+++ bnichs/src/components/QRCode.vue
@@ -3,13 +3,14 @@ import QrcodeVue from 'qrcode.vue'
 </script>
 
 <template>
-    <qrcode-vue class="qr-box" :value="value" :size="size" level="H" />
+    <qrcode-vue class="qr-box" :value="value" :size="size" :foreground="fgColor" :background="bgColor" level="H" />
 </template>
 
 <script lang="ts">
 import {PERSON_WEBSITE} from "@/config";
+import {defineComponent} from "vue";
 
-export default {
+export default defineComponent({
   props: {
     size: {
       type: Number,
@@ -19,12 +20,45 @@ export default {
   data() {
     return {
       value: PERSON_WEBSITE,
-      // size: 55,
+      fgColor: colorFromCSSClass("qrForeground"),
+      bgColor: colorFromCSSClass("qrBackground")
     }
   },
-}
+  computed: {
+  },
+  methods: {
+    updateQR(){
+      this.fgColor = colorFromCSSClass("qrForeground")
+      this.bgColor = colorFromCSSClass("qrBackground")
+    }
+  },
+  mounted(){
+    let outerThis = this
+
+    // Update qr code when the theme changes
+    let observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        if (mutation.type === "attributes") {
+          outerThis.updateQR()
+        }
+      });
+    });
+
+    let element = document.documentElement
+    observer.observe(element, {
+      attributes: true //configure it to listen to attribute changes
+    });
+  }
+})
 </script>
 
 
 <style>
+.qrForeground{
+  color: var(--color-link);
+}
+
+.qrBackground{
+  color: var(--color-background);
+}
 </style>
