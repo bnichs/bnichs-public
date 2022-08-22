c7fc1e68d28e5f4783a67551a948d84ba9d18132
Ben <mail@ben-nichols.net>
2022-08-22 14:07:15 -0700

    Install fonts in build

--- bnichs/vite.config.ts
+++ bnichs/vite.config.ts
@@ -13,37 +13,6 @@ function buildBlog () {
     },
   };
 }
-//
-// function flattenSVG (svg) {
-//   const images = svg.match(/<image [^>]+>/g);
-//   if (!images || images.length < 1) {
-//     return svg;
-//   }
-//
-//   var result = svg;
-//   images.forEach(image => {
-//     const [,data] = image.match(/ xlink:href="data:image\/svg\+xml;base64,([^"]+)"/) || [];
-//     if (!data) {
-//       return;
-//     }
-//
-//     const innerSVG = Buffer.from(data, 'base64').toString();
-//     const [,width] = image.match(/ width="([^"]+)"/) || [];
-//     const [,height] = image.match(/ height="([^"]+)"/) || [];
-//     const [,opacity] = image.match(/ opacity="([^"]+)"/) || [];
-//     const [,x] = image.match(/ x="([^"]+)"/) || [];
-//     const [,y] = image.match(/ y="([^"]+)"/) || [];
-//     const [header] = innerSVG && innerSVG.match(/<svg[^>]+>/) || [];
-//     const fixedHeader = header
-//         .replace(/ (x|y|width|height)="([^"]+)"/g, '')
-//         .replace('<svg', `<svg x="${x}" y="${y}" width="${width}" height="${height}" opacity="${opacity || 1.0}"`);
-//     const replacement = innerSVG && innerSVG.replace(header, fixedHeader);
-//     result = result.replace(image, replacement);
-//   });
-//
-//   return result;
-// }
-
 
 // Convert svg to pngs and favicon
 function convertImages () {
